0c1aa17fc798c9023b43f532846ab290d773fbec
magarcd <manuelbadman@gmail.com>
2019-11-06 18:09:08 +0100

    included some advances in the development of the proyect

---
+++ evaluateModel.py
@@ -0,0 +1,28 @@
+import argparse
+import factoryModel
+from conf import Conf
+
+
+def main():
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-f", "--framework", required=True, help="framework of the object detection model")
+    ap.add_argument("-m", "--model", required=True, help="deep learning object detection model")
+    ap.add_argument("-d", "--dataset", required=True, help="path of the dataset")
+    ap.add_argument("-dn", "--dataset_name", required=True, help="name of the dataset")
+    ap.add_argument("-ng", "--ngpus", required=False, help="number of gpus to use")
+
+    args = vars(ap.parse_args())
+
+    framework = args["framework"]
+    modelText = args["model"]
+    dataset = args["dataset"]
+    dataset_name = args["dataset_name"]
+    ngpus = int(args["ngpus"])
+
+
+    conf = Conf("./config_framework.json")
+
+    model = factoryModel.createModel(framework, modelText, dataset, dataset_name)
+    model.evaluate()
+if __name__ == "__main__":
+    main()
